b70099fb7761dc06d569a7293a750bbe9432cd90
bigfishh <anniezheng.anniee@gmail.com>
2020-10-15 21:18:58 -0400

    week2

---
+++ week2/index.js
@@ -0,0 +1,14 @@
+// Write a function called
+//  `hasTargetSum` that receives two arguments:
+//    * an array of integers
+//    * a target integer
+
+//  The function should return all pairs of numbers found in the array that add up to the target number.
+//  hasTargetSum([-1, 3, 8, 12, 4, 11, 7], 10) ==>  [[-1, 11], [3, 7]]
+
+function hasTargetSum(arr, sum) {
+
+
+}
+
+console.log(hasTargetSum([-1, 3, 8, 12, 4, 11, 7], 10)) // ==>  [[-1, 11], [3, 7]]
