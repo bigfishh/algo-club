dd9335ba2df54b6a2c9d1137b7d408a2cab9ca7a
bigfishh <anniezheng.anniee@gmail.com>
2020-10-22 11:12:00 -0400

    updates week 3 material

--- week3/hashtable.js
+++ week3/hashtable.js
@@ -1,3 +1,22 @@
+//Google Question
+//Given an array = [2,5,1,2,3,5,1,2,4]:
+//It should return 2
+
+//Given an array = [2,1,1,2,3,5,1,2,4]:
+//It should return 1
+
+//Given an array = [2,3,4,5]:
+//It should return undefined
+
+
+function firstRecurringCharacter(input) {
+
+}
+
+
+
+firstRecurringCharacter([2,5,5,2,3,5,1,2,4]) // 5
+
 
 
 // Exercise: Ransom Note
